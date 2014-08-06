12ad82044e94cd7774921a97cf09c13cf6086225
Sam Tape <stape@fisdap.net>
2014-08-06 10:52:25 -0500

    Finishing chapter 14 of 'Build Larabook From Scratch' tutorial on Laracasts. Adding login/logout functionality.

---
+++ app/controllers/SessionsController.php
@@ -0,0 +1,61 @@
+<?php
+
+use Larabook\Forms\SignInForm;
+
+class SessionsController extends \BaseController {
+
+    /**
+     * @var SignInForm
+     */
+    private $signInForm;
+
+    function __construct(SignInForm $signInForm)
+    {
+        $this->beforeFilter('guest', ['except' => 'destroy']);
+
+        $this->signInForm = $signInForm;
+    }
+
+	/**
+	 * Show the form for signing in.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return View::make('sessions.create');
+	}
+
+
+	/**
+	 * Authenticate the user
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//fetch the form input
+        $formData = Input::only('email', 'password');
+
+        //validate the form
+        //if invalid, then go back
+        $this->signInForm->validate($formData);
+
+        //if is valid, then try to sign in
+        if (Auth::attempt($formData)) {
+            //redirect to statuses
+            Flash::message('Welcome Back!');
+            return Redirect::intended('/statuses');
+        }
+	}
+
+    /**
+     * Logout the user
+     */
+    public function destroy()
+    {
+        Auth::logout();
+        Flash::message('You have now been logged out.');
+        return Redirect::home();
+    }
+}
--- tests/_support/FunctionalHelper.php
+++ tests/_support/FunctionalHelper.php
@@ -1,10 +1,30 @@
-<?php
-namespace Codeception\Module;
+<?php namespace Codeception\Module;
+
+Use Laracasts\TestDummy\Factory as TestDummy;
 
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
 class FunctionalHelper extends \Codeception\Module
 {
+    public function haveAnAccount($overrides = [])
+    {
+        TestDummy::create('Larabook\Users\User', $overrides);
+    }
+
+    public function signIn()
+    {
+        $email = 'foo@example.com';
+        $password = 'demo';
+
+        $this->haveAnAccount(['email'=> $email, 'password' => $password]);
+        $I= $this->getModule('Laravel4');
+
+
+        $I->amOnPage('/login');
+        $I->fillField('email', $email);
+        $I->fillField('password', $password);
+        $I->click('Sign In');
+    }
 
 }
