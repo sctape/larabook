f96fde63e40c5be6fe80bb92e84f4473692f1bbb
Sam Tape <stape@fisdap.net>
2014-08-07 17:16:00 -0500

    Finishing chapter 18 of 'Build Larabook From Scratch' tutorial on Laracasts. Adding user presentation logic using gravatar.

--- app/Larabook/Users/User.php
+++ app/Larabook/Users/User.php
@@ -7,10 +7,11 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 use Eloquent, Hash;
 use Larabook\Registration\Events\UserRegistered;
 use Laracasts\Commander\Events\EventGenerator;
+use Laracasts\Presenter\PresentableTrait;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	use UserTrait, RemindableTrait, EventGenerator;
+	use UserTrait, RemindableTrait, EventGenerator, PresentableTrait;
 
     /**
      * @var array Which fields may be mass assigned?
@@ -23,6 +24,13 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 * @var string
 	 */
 	protected $table = 'users';
+
+    /**
+     * Path to the presenter for a user
+     *
+     * @var string
+     */
+    protected $presenter = 'Larabook\Users\UserPresenter';
 
 	/**
 	 * The attributes excluded from the model's JSON form.
